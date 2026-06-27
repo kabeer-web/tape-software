688cbf970a45348105eb5e2923d0094e31af42b3
Kabeer <kabeer@example.com>
2026-06-27 15:15:18 +0500

    Add environment variables support

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,7 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'https://gxxndoypcrgfqkqwesni.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_lPUaMlpgGBNhTQ8rEfPVKg_I3xn2x5A';
+// Ye tareeqa local aur online dono jagah kaam karega
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
