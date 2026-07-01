0ee3c4498cfb91b9ce0cbf5ffd8b0418094cc5df
kabeerthenix <kabeerai9090@gmail.com>
2026-07-01 10:03:25 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'; // Agar aapki CSS file hai to
-import { StockProvider } from "./Components/inventory/StockContext"; // Ensure folder is "Components" with Capital C
+import './index.css';
+import { StockProvider } from "./Components/inventory/StockContext";
+import { AuthProvider } from './Components/AuthContext';
+import { AccountsProvider } from './Components/inventory/ACCOUNTS/AccountsContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <StockProvider>
-      <App />
-    </StockProvider>
+    <AuthProvider>
+      <StockProvider>
+        <AccountsProvider>
+          <App />
+        </AccountsProvider>
+      </StockProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
